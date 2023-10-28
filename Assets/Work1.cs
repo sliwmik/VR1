c4dab722c076aa631a015a465b0109ad579c42ff
sliwmik <144949529+sliwmik@users.noreply.github.com>
2023-10-28 13:04:26 +0300

    Viar

--- Assets/Work1.cs
+++ Assets/Work1.cs
@@ -5,25 +5,72 @@ using UnityEngine.UI;
 
 public class Work1 : MonoBehaviour
 {
+    public float Speed = 10f;
     public int a;
     public int b;
     public int c;
     public GameObject sphere;
     public Text x;
-    public GameObject camer;
-
+    
     void Start()
     {
 
     }
     void FixeUpDate()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.W))
         {
-            a = Random.Range(0, 255);
-            b = Random.Range(0, 255);
-            c = Random.Range(0, 255);
-            this.gameObject.GetComponent<Renderer>().material.color = new Color32((byte)a, (byte)b, (byte)c, 1);
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
         }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down *5* Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * Speed * Time.deltaTime);
+        }
+            if (c <= 0)
+        {
+            this.gameObject.SetActive(false);
+            x.text = "Игра окончена";
+
+        }
+        else x.text = c.ToString();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("CEN");
+        this.gameObject.GetComponent<Renderer>().material.color = Color.red;
+        c--;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("CET");
+        this.gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("CS");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TE");
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("TX");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("TS");
+    }
+    public void heal()
+    {
+        c = 5;
     }
 }
